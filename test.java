00f0c2f8cc101dbc34a4089a1cfca05bc14286ad
Ruan-car <00001084261418sp@al.educacao.sp.gov.br>
2024-09-19 07:46:52 -0300

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,11 @@
+const avanca = document.querySelectorAll('.botão');
 
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('botão');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
